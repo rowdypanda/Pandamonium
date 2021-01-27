422899265ca6489ee8d9d29af4d4f496ab030e38
rowdypanda <sejong2992@gmail.com>
2021-01-27 03:22:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import discord
+import os
+import requests
+import json
+import random
+from replit import db
+
+client = discord.Client()
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "dejected", "sorrowful", "despondent", "out of sorts", "saddened", "gloomy", "glum", "melancholy", "disconsolate", "downhearted", "downcast", "cast down"]
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!",
+  "Optimism is the faith that leads to achievement - Helen Keller",
+  "You got this.",
+  "I know this won't be easy, but I know you've also got what it takes to get through it.",
+  "Sending you good thoughts - and hoping you believe in yourself just as much as I believe in you."
+]
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+def update_enouragement(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else: 
+    db["encouragements"] = [encouraging_message]
+
+def delete_encouragement(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+  db["encouragements"] = encouragements
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+  
+  if any(word in msg for word in sad_words) :
+    await message.channel.send(random.choice(starter_encouragements))
+
+client.run(os.getenv('TOKEN'))
